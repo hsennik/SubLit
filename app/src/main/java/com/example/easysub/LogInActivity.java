65de3377e7914766a605561b817124492e1e8ab6
Hannah Sennik <hsennik@uwaterloo.ca>
2016-09-17 15:01:35 -0400

    Changed log in page to go straight to user

--- app/src/main/java/com/example/easysub/LogInActivity.java
+++ app/src/main/java/com/example/easysub/LogInActivity.java
@@ -16,5 +16,6 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
