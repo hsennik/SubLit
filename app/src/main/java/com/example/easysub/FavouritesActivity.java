bfb746130df1c910d30c819ae788430493ec4188
Johvonna Murray-Bradshaw <johvonnamb_@hotmail.com>
2016-09-18 01:50:23 -0400

    - List in saved locations w/ arbitrary data

--- app/src/main/java/com/example/easysub/FavouritesActivity.java
+++ app/src/main/java/com/example/easysub/FavouritesActivity.java
@@ -1,5 +1,7 @@
 package com.example.easysub;
 
+import android.app.Activity;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
 import android.app.ListActivity;
@@ -9,31 +11,72 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.view.View;
+import android.widget.Toast;
 
 
 /**
  * Created by Lenavo on 2016-09-17.
  */
 
-public class FavouritesActivity extends AppCompatActivity {
+public class FavouritesActivity extends Activity {
+    ListView listView ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourites);
 
-        //An array of favourite sublets (strings)
-        String [] favSubletName = {"University of Waterloo","203 Lester Street","16 Ezra Avenue","17 Young Street East","111 Keats Place"};
-        Boolean [] favSubletBool = {true, false, false, true, true}; //CHANGE TO ACQUIRE DATA INPUT ON SEARCH ACTIVITY
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
 
-        //CONVERT INTO LIST FORMAT
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(), R.id.favList, favSubletName);
-        getListView().setAdapter(adapter);
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
 
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
     }
 
-
-
 }
-
