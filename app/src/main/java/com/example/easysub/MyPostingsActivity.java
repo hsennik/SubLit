f8f72e8b0061a63d50a989b5f090df63a3144966
Johvonna Murray-Bradshaw <johvonnamb_@hotmail.com>
2016-09-18 05:21:06 -0400

    small changesupdate to settings and mypostings

--- app/src/main/java/com/example/easysub/MyPostingsActivity.java
+++ app/src/main/java/com/example/easysub/MyPostingsActivity.java
@@ -1,8 +1,10 @@
 package com.example.easysub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Lenavo on 2016-09-17.
@@ -12,5 +14,11 @@ public class MyPostingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypostings);
+
+        Button btn_mysublet = (Button) findViewById(R.id.mysublet_button);
+        btn_mysublet.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                startActivity(new Intent(MyPostingsActivity.this, PostingActivity.class));}
+        });
     }
 }
